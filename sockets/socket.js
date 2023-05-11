de2b2e95f26e94dcf2adccf21dab3b560c0b2449
Samyak Panchbhai <Samyak.Panchbhai@ril.com>
2023-05-11 14:45:32 +0530

    updated code

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,14 +1,14 @@
 import socketIO, { Socket } from "socket.io";
 import { User } from "../user";
 
-export const disconnectClient = (client: Socket, io: socketIO.Server) => {
+export const disconnectClient = (client, io) => {
   client.on("disconnect", () => {
     User.removeUser(client.id);
     io.emit("users-online", User.getUserList());
   });
 };
 
-export const addUserOnline = (client: Socket, io: socketIO.Server) => {
+export const addUserOnline = (client, io) => {
   client.on("add-user", (payload) => {
     payload.id = client.id;
     User.addUser(payload);
@@ -16,7 +16,7 @@ export const addUserOnline = (client: Socket, io: socketIO.Server) => {
     io.emit("users-online", User.getUserList());
   });
 };
-export const removeUserOnline = (client: Socket, io: socketIO.Server) => {
+export const removeUserOnline = (client, io) => {
   client.on("exit", () => {
     User.removeUser(client.id);
     io.emit("users-online", User.getUserList());
